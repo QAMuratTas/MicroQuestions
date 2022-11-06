5fd288f3ea11dfd6c8dd56bbabd642b4fb346f9a
QA Murat Tas <itmmt38@gmail.com>
2022-11-06 15:57:56 +0300

    arraylist

--- src/Micro/Arraylist.java
+++ src/Micro/Arraylist.java
@@ -1,2 +1,17 @@
-package Micro;public class Arraylist {
-}
+package Micro;
+
+import java.util.ArrayList;
+
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+
+        for (int i = 0; i <=100 ; i+=2) {
+            myNumbers.add(i);
+
+        }
+
+        System.out.println("myNumbers = " + myNumbers);
+    }
+    }
+
